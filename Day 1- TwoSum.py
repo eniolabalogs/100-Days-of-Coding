777e218b010242787bbfce91898a6cdf124cc816
Oyindamola Balogun <dbalogun2023@gmail.com>
2024-05-09 00:47:36 -0500

    updated the code to the correct one

--- Day 1- TwoSum.py
+++ Day 1- TwoSum.py
@@ -7,9 +7,22 @@ def TwoSum(n,t):
 TwoSum([3,3],(6))
 '''
 
+"""
 def TwoSum(n,t):
     for i in n:
         diff= t-i 
         if diff in n:
             print (diff, i)
-TwoSum([2,7,11,15],(9))          
+TwoSum([2,7,11,15],(9))    
+"""
+
+def TwoSum(n,t):
+    diction={}
+    for i, a in enumerate(n):
+        d= t- a
+        if d in diction:
+            return [diction[d],i]
+        else:
+            diction[a]=i
+
+print(TwoSum(([2,7,11,15]),(9)))
