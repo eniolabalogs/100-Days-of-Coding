65d242e243395bd87f3c8b82c289ff93be984f71
Oyindamola Balogun <dbalogun2023@gmail.com>
2024-05-13 23:30:28 -0500

    Started Day 4 project

---
+++ Day 4- Best time to buy and Sell Stock.py
@@ -0,0 +1,20 @@
+def maxProfit(prices):
+    """
+    n=prices[0]
+    a=[]
+    for i in prices:
+        if i<n:
+            n=i
+            a=[]
+            a.append(n)
+            b=a.index(n)
+                
+        else:
+            a.append(i)
+    c=max(a)
+    return c-n
+    """
+
+    for i in 
+
+print(maxProfit([7,1,5,3,6,4]))
